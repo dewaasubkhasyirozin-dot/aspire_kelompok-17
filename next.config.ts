6e9321e890c2b19de4da93088fff331dc4c56d1e
dewaasubkhasyirozin-dot <dewaasubkhasyirozin@gmail.com>
2026-05-22 22:30:38 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
+    remotePatterns: [{ protocol: "https", hostname: "**" }],
+  },
+  allowedDevOrigins: ["192.168.1.4", "localhost"],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
